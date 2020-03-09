937126faafa2220f60f590135a54d8f4e8b0c189
ilhamfirdaa <ilhamfirdaa@gmail.com>
2020-03-10 02:24:39 +0700

    add routes for each pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import Home from './components/Home'
+import Continent from './components/Continent'
+import Area from './components/Area'
+import League from './components/League'
+import Club from './components/Club'
+import Player from './components/Player'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/continent/:continentId" component={Continent} />
+      <Route path="/area/:areaId" component={Area} />
+      <Route path="/league/:leagueId" component={League} />
+      <Route path="/club/:clubId" component={Club} />
+      <Route path="/player/:playerId" component={Player} />
+    </Switch>
+  )
 }
 
-export default App;
+export default App
